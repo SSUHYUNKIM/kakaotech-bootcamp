d141f8c7a95eaee1ad5666cb73f4820c9e3ef8dc
SSUHYUNKIM <yellosuhyun@naver.com>
2024-07-05 17:01:03 +0900

    feat : 할 일 수정하기

--- ToDoList/script.js
+++ ToDoList/script.js
@@ -14,20 +14,60 @@ function updateIncompleteCount() {
 
 function addTask() {
     if(inputBox.value === "") {
-        alert("Please enter a task");
+        alert("할 일을 작성해주세요!");
     }
     else {
         const li = document.createElement("li");
         li.innerHTML = inputBox.value;
+        li.classList.add('unchecked');
         listContainer.appendChild(li);
         inputBox.value = "";
+
+        const editBtn = document.createElement("edit");
+        editBtn.innerHTML = "\u270E";
+        editBtn.onclick = function() { editTask(li, editBtn); };
+        li.appendChild(editBtn);
+
         const span = document.createElement("span");
         span.innerHTML = "\u00d7";
+        span.onclick = function() { li.remove(); updateIncompleteCount(); saveData(); };
         li.appendChild(span);
     }
     inputBox.value = "";
     saveData();
     updateIncompleteCount();
+}
+
+function editTask(li, editBtn) {
+    const currentText = li.childNodes[0].nodeValue.trim();
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = currentText;
+    li.insertBefore(input, li.firstChild);
+    li.removeChild(li.childNodes[1]);
+    const span = li.querySelector("span");
+    editBtn.innerHTML = "\u2713";
+
+    editBtn.onclick = function() {
+        li.textContent = input.value;
+        li.appendChild(editBtn);
+        li.appendChild(span);
+        editBtn.innerHTML = "\u270E";
+        editBtn.onclick = function() { editTask(li, editBtn); };
+        updateIncompleteCount();
+        saveData();
+    };
+}
+
+function setActiveButton(buttonId) {
+    const buttons = document.querySelectorAll('.filter-btn');
+    buttons.forEach(button => {
+        if (button.id === buttonId) {
+            button.classList.add('active');
+        } else {
+            button.classList.remove('active');
+        }
+    });
 }
 
 function filterTasks(filter) {
@@ -36,10 +76,11 @@ function filterTasks(filter) {
         task.classList.remove('hidden');
         if (filter === 'active' && task.classList.contains('checked')) {
             task.classList.add('hidden');
-        } else if (filter === 'completed' && task.classList.contains('unchecked')) {
+        } else if (filter === 'completed' && !task.classList.contains('checked')) {
             task.classList.add('hidden');
         }
     }
+    setActiveButton(filter); // 필터링 후 활성 버튼 설정
 }
 
 function deleteAllTasks() {
@@ -60,6 +101,7 @@ listContainer.addEventListener('click', function(e) {
             e.target.classList.remove('checked');
             e.target.classList.add('unchecked');
         } else {
+            console.log("test");
             e.target.classList.remove('unchecked');
             e.target.classList.add('checked');
         }
